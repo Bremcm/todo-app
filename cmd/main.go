57098588e269e41bbf0aa91a7c66c14fdb8183ab
bremcm <281910767+Bremcm@users.noreply.github.com>
2026-06-20 00:15:44 +0200

    intialization all dependencies in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/bremcm/todo-app"
 	"github.com/bremcm/todo-app/pkg/handler"
+	"github.com/bremcm/todo-app/pkg/handler/service"
+	"github.com/bremcm/todo-app/pkg/handler/service/repository"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
