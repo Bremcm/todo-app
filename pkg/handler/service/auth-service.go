6985064eaf9acc42900a529a9541f7aede5624fb
bremcm <281910767+Bremcm@users.noreply.github.com>
2026-06-22 02:39:40 +0300

    feat(auth): implement JWT generation and user authentication

--- pkg/handler/service/auth-service.go
+++ pkg/handler/service/auth-service.go
@@ -1,10 +1,24 @@
 package service
 
 import (
+	"errors"
+	"time"
+
 	"github.com/bremcm/todo-app"
 	"github.com/bremcm/todo-app/pkg/handler/service/repository"
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const (
+	signingKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +35,28 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	}
 	user.Password = hash
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username)
+	if err != nil {
+		return "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return "", errors.New("invalid username or password")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func (s *AuthService) generatePasswordHash(password string) (string, error) {
