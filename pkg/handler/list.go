e8623b8b44e57079309de8d09beed195cdc9545f
bremcm <281910767+Bremcm@users.noreply.github.com>
2026-06-23 01:30:04 +0300

    Add create todo list repository and service

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,14 +3,23 @@ package handler
 import (
 	"net/http"
 
+	"github.com/bremcm/todo-app"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
-	c.JSON(http.StatusOK, map[string]interface{}{
-		"id": id,
-	})
+	_, ok := c.Get(userCtx)
+	if !ok {
+		newErrorResponce(c, http.StatusInternalServerError, "user id not found")
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponce(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
 }
 func (h *Handler) getAllList(c *gin.Context) {
 
