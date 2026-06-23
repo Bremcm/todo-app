8e948e322824d5a186e44363130395431ced4547
bremcm <281910767+Bremcm@users.noreply.github.com>
2026-06-23 23:23:41 +0300

    feat(list): implement update list endpoint with validation

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -72,9 +72,33 @@ func (h *Handler) getListById(c *gin.Context) {
 
 	c.JSON(http.StatusOK, list)
 }
-func (h *Handler) updateList(c *gin.Context) {
 
+func (h *Handler) updateList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponce(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	var input todo.UpdateListInput
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponce(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.services.TodoList.Update(userId, id, input); err != nil {
+		newErrorResponce(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{Status: "ok"})
 }
+
 func (h *Handler) deleteList(c *gin.Context) {
 	userId, err := getUserId(c)
 	if err != nil {
