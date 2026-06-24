c5e5ebf75353bcbc06fb783718cbdf020f280859
bremcm <281910767+Bremcm@users.noreply.github.com>
2026-06-24 18:08:17 +0300

    feat(item): implement get item by id endpoint

--- pkg/handler/service/service.go
+++ pkg/handler/service/service.go
@@ -22,6 +22,7 @@ type TodoList interface {
 type TodoItem interface {
 	Create(userId, listId int, item todo.TodoItem) (int, error)
 	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId, itemId int) (todo.TodoItem, error)
 }
 
 type Service struct {
@@ -34,5 +35,6 @@ func NewService(repos *repository.Repository) *Service {
 	return &Service{
 		Authorization: NewAuthService(repos.Authorization),
 		TodoList:      NewTodoListService(repos.TodoList),
+		TodoItem:      NewTodoItemService(repos.TodoItem, repos.TodoList),
 	}
 }
